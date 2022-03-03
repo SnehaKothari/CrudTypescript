b6899de43277128ea4f18a85cdc36e151a055ed8
SnehaKothari <73631603+SnehaKothari@users.noreply.github.com>
2022-03-03 14:45:08 +0530

    Update Home.tsx

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
 import { UserProps} from '../Pages/User';
 
 
 interface HomeProps{
     users:UserProps[];
-  
-  
 }
 
 const Home = () => {
@@ -17,24 +14,17 @@ const[users,setUser]=useState<UserProps[]>([]);
     loadUsers();
   }, []);
 
-  const loadUsers=async()=>{
-    
-  const res=await axios.get("http://localhost:3003/users");
-        
-        setUser(res.data);
-       
-    }
+const loadUsers=async()=>{
+const res=await axios.get("http://localhost:3003/users");
+    setUser(res.data);
+}
+const deleteUserData=async(user:UserProps)=>{
+await axios.delete(`http://localhost:3003/users/${user.id}`)
+    loadUsers();
+  }
   
-    
-    
-    const deleteUserData=async(user:UserProps)=>{
-    await axios.delete(`http://localhost:3003/users/${user.id}`) 
-      loadUsers();
-    
-    }
-  
-    return(
-        <div className="container">
+return(
+      <div className="container">
       <div className="py-4">
         <h1>User Details</h1>
        
